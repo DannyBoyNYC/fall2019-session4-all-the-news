d27119dd47bde1b32d9126d5e16c0beb96042d2a
DannyBoyNYC <daniel.deverell@gmail.com>
2019-10-31 19:52:18 -0400

    done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+
+const navItems = [
+  {
+    label: 'arts',
+    link: '#arts',
+  },
+  {
+    label: 'books',
+    link: '#books',
+  },
+  {
+    label: 'fashion',
+    link: '#fashion',
+  },
+  {
+    label: 'food',
+    link: '#food',
+  },
+  {
+    label: 'movies',
+    link: '#movies',
+  },
+  {
+    label: 'travel',
+    link: '#travel',
+  },
+];
 
 function App() {
   return (
-    <div>
-      <h4>Let's go!</h4>
-    </div>
+    <>
+      <Header siteTitle="All the News That Fits We Print" />
+      <Nav navItems={navItems} />
+    </>
   );
 }
 
